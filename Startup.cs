03edf989bc897e48f0c2d2979cab3358eb99115a
palwinder sandhu <palwindersandhu367@gmail.com>
2019-06-26 10:21:59 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchedularApp.Startup))]
+namespace SchedularApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
